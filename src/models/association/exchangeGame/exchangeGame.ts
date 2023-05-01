dca9d6e714047cc7021621320e270ef648c4b6c2
Eric Barch <ericmb365@gmail.com>
2023-05-01 11:46:25 -0400

    Minor refactor of models.exchangeGame.exchangeModels

--- src/models/association/exchangeGame/exchangeGame.ts
+++ src/models/association/exchangeGame/exchangeGame.ts
@@ -6,8 +6,8 @@ import * as models from '../../../models';
 export abstract class ExchangeGame {
     private wrappedExchange: models.Exchange;
     private wrappedGame: models.Game;
-    public wrappedExchangeGameAwayTeam: models.ExchangeGameTeam | null;
-    public wrappedExchangeGameHomeTeam: models.ExchangeGameTeam | null;
+    protected wrappedExchangeGameAwayTeam: models.ExchangeGameTeam | null;
+    protected wrappedExchangeGameHomeTeam: models.ExchangeGameTeam | null;
 
     protected constructor({
         exchange,
