cca53504af3ce4e2019c8847828a2e5599d2570a
Eric Barch <ericmb365@gmail.com>
2023-05-24 11:56:33 -0400

    Implement first jest test

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
 };
---
+++ tests/parsers/exchange-models/draft-kings/games-page-parser/nba-games-page-parser.test.ts
@@ -0,0 +1,92 @@
+import { DraftKingsNbaGamesPageParser } from '../../../../../src/parsers/exchange-models/draft-kings/games-page-parser/nba-games-page-parser';
+import { JsonGamesParser } from '../../../../../src/parsers/exchange-models/draft-kings/games-page-parser/json-games-parser';
+import { DocumentGamesParser } from '../../../../../src/parsers/exchange-models/draft-kings/games-page-parser/document-games-parser';
+
+jest.mock('../../../../../src/parsers/exchange-models/draft-kings/games-page-parser/json-games-parser');
+jest.mock('../../../../../src/parsers/exchange-models/draft-kings/games-page-parser/document-games-parser');
+
+describe('DraftKingsNbaGamesPageParser', () => {
+  let draftKingsNbaGamesPageParser: DraftKingsNbaGamesPageParser;
+  
+  const mockJsonGames = [
+    {
+      id: 1,
+      startDate: new Date(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      awayTeamId: 1,
+      homeTeamId: 2,
+    },
+    {
+      id: 2,
+      startDate: new Date(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      awayTeamId: 3,
+      homeTeamId: 4,
+    },
+    {
+      id: 3,
+      startDate: new Date(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      awayTeamId: 5,
+      homeTeamId: 6,
+    },
+  ];
+  const mockDocumentGames = [
+    {
+      id: 1,
+      startDate: new Date(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      awayTeamId: 1,
+      homeTeamId: 2,
+    },
+    {
+      id: 2,
+      startDate: new Date(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      awayTeamId: 3,
+      homeTeamId: 4,
+    },
+  ];
+
+  beforeEach(() => {
+    // @ts-ignore
+    JsonGamesParser.mockImplementation(() => {
+      return {
+        getGames: jest.fn().mockResolvedValue(mockJsonGames),
+      };
+    });
+
+    // @ts-ignore
+    DocumentGamesParser.mockImplementation(() => {
+      return {
+        getGames: jest.fn().mockResolvedValue(mockDocumentGames),
+      };
+    });
+
+    draftKingsNbaGamesPageParser = new DraftKingsNbaGamesPageParser();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('getGames', () => {
+    it('should return games from documentGamesParser', async () => {
+      const games = await draftKingsNbaGamesPageParser.getGames();
+      expect(games).toEqual(mockDocumentGames);
+    });
+
+    it('should not return games that are returned from jsonGamesParser but not from documentGamesParser', async () => {
+      const games = await draftKingsNbaGamesPageParser.getGames();
+      
+      // The game with id 3 is in jsonGamesParser but not in documentGamesParser
+      const gameNotInDocumentGames = mockJsonGames.find(game => game.id === 3);
+      expect(games).not.toContainEqual(gameNotInDocumentGames);
+    });
+  });
+});
