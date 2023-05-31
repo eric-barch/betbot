b3798f7a956bb26d75137e350b9988e888a7ed0a
Eric Barch <ericmb365@gmail.com>
2023-05-31 10:20:23 -0400

    Minor refactor

--- src/db/wrappers/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './exchange-wrapper';
-export * from './league-wrapper';
---
+++ src/init-data/exchanges/fan-duel.ts
@@ -0,0 +1,3 @@
+import { ExchangeInitData } from './exchange-init-data';
+
+export const fanDuel = new ExchangeInitData({ name: 'FanDuel' });
