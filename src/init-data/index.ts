e879e4308d221bef0e2c765039b229ee31853081
Eric Barch <ericmb365@gmail.com>
2023-05-31 11:10:10 -0400

    Init teams working

--- src/init-data/index.ts
+++ src/init-data/index.ts
@@ -1,4 +1,3 @@
 export * from './exchanges';
 export * from './leagues';
 export * from './teams';
-export * from './league-teams';
--- src/init-data/league-teams/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './league-teams-init-data';
-export * from './mlb-teams';
-export * from './nba-teams';
-export * from './nfl-teams';
