fcceed1d0d90993c2bf766ae69dc65cce2e9589c
Eric Barch <ericmb365@gmail.com>
2023-06-13 15:28:35 -0400

    Implement FanDuelDbGameInitializer

--- src/parsers/models/exchange-models/fan-duel/fan-duel-page-parser/fan-duel-odd-button-parsers/fan-duel-odd-button-parser/fan-duel-db-initializers/fan-duel-db-game-initializer/fan-duel-db-game-initalizer.ts
+++ src/parsers/models/exchange-models/fan-duel/fan-duel-page-parser/fan-duel-odd-button-parsers/fan-duel-odd-button-parser/fan-duel-db-initializers/fan-duel-db-game-initializer/fan-duel-db-game-initalizer.ts
@@ -1,6 +1,7 @@
 import { Game } from '@prisma/client';
 
 import { DbGameInitializer, OddButtonParser } from '@/parsers/models/shared-models';
+import { prisma } from '@/db';
 
 export class FanDuelDbGameInitializer extends DbGameInitializer {
   private wrappedExchangeAssignedGameId: string | undefined;
@@ -20,15 +21,68 @@ export class FanDuelDbGameInitializer extends DbGameInitializer {
     await this.parseExchangeAssignedGameId();
 
     try {
-      // this.game = await this.findGameWithExchangeAssignedId();
+      this.game = await this.findGameWithExchangeAssignedId();
     } catch {
-      //this.game = await this.findOrCreateGameWithoutExchangeAssignedId();
+      this.game = await this.findOrCreateGameWithoutExchangeAssignedId();
     }
-
     return this.game;
   }
 
   private async parseExchangeAssignedGameId(): Promise<string> {
-    throw new Error(`parseExchangeAssignedGameId not implemented.`);
+    const button = this.parentOddButtonParser.button;
+
+    if (!button) {
+      throw new Error(`button is null.`);
+    }
+
+    const linkElement = await button.$('xpath/../../../a');
+
+    if (!linkElement) {
+      throw new Error(`linkElement is null.`);
+    }
+
+    const href = await linkElement.evaluate(el => el.getAttribute('href'));
+
+    if (!href) {
+      throw new Error(`href is null.`);
+    }
+
+    const lastHyphenPos = href.lastIndexOf('-');
+
+    this.exchangeAssignedGameId = href.substring(lastHyphenPos + 1);
+    return this.exchangeAssignedGameId;
+  }
+
+  private async findGameWithExchangeAssignedId(): Promise<Game> {
+    const exchangeToGame = await prisma.exchangeToGame.findUniqueOrThrow({
+      where: {
+        exchangeId_exchangeAssignedGameId: {
+          exchangeId: this.parentOddButtonParser.exchange.id,
+          exchangeAssignedGameId: this.exchangeAssignedGameId,
+        },
+      },
+      include: {
+        game: true,
+      },
+    });
+
+    this.game = exchangeToGame.game;
+    return this.game;
+  }
+
+  private async findOrCreateGameWithoutExchangeAssignedId(): Promise<Game> {
+    throw new Error(`findOrCreateGameWithoutExchangeAssignedId not implemented.`);
+  }
+
+  private set exchangeAssignedGameId(exchangeAssignedGameId: string) {
+    this.wrappedExchangeAssignedGameId = exchangeAssignedGameId;
+  }
+
+  private get exchangeAssignedGameId(): string {
+    if (!this.wrappedExchangeAssignedGameId) {
+      throw new Error(`wrappedExchangeAssignedGameId is undefined.`);
+    }
+
+    return this.wrappedExchangeAssignedGameId;
   }
 }
