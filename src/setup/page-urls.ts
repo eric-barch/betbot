872b15840258c586cd4a0fa0cff29c096e07154d
Eric Barch <ericmb365@gmail.com>
2023-07-17 22:52:12 -0400

    Implement SugarHouseParserFactory

--- src/setup/page-urls.ts
+++ src/setup/page-urls.ts
@@ -5,7 +5,7 @@ export const pageUrls = [
   // 'https://sportsbook.fanduel.com/navigation/mlb',
   // 'https://sportsbook.fanduel.com/navigation/nba',
   // 'https://sportsbook.fanduel.com/navigation/nfl',
-  // 'https://ct.playsugarhouse.com/?page=sportsbook&group=1000093616&type=matches#home',
+  'https://ct.playsugarhouse.com/?page=sportsbook&group=1000093616&type=matches#home',
   // 'https://ct.playsugarhouse.com/?page=sportsbook&group=1000093652&type=matches#home',
   // 'https://ct.playsugarhouse.com/?page=sportsbook&group=1000093656&type=matches#home',
 ];
