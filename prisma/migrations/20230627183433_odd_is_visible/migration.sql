9a520115ad1e24f7e3e909a4ffa1393a0bcc8b14
Eric Barch <ericbarch@Erics-MacBook-Pro.local>
2023-07-20 00:59:07 -0400

    Preliminary Supabase setup

--- prisma/migrations/20230627183433_odd_is_visible/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `isVisible` to the `Odd` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Odd" ADD COLUMN     "isVisible" BOOLEAN NOT NULL;
